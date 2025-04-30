9a98d05132cd04b73b0804184179b1eedfb11e62
Ars3nna <zmaxwell.green@yahoo.com>
2025-04-30 18:53:13 +0100

    Updated merchant dialogue window

--- Scripts/dialogue_merchant.gd
+++ Scripts/dialogue_merchant.gd
@@ -5,18 +5,31 @@ var dialogue = []
 var current_dialogue_id = 0
 var d_active = false
 var first_dialogue = true
+var close_script =  false
 
 func _ready() -> void:
 	$NinePatchRect.visible = false
+	$"Yes button rect".visible = false
+	$"No button rect".visible = false
+	$"Yes button rect/Yes button".disabled = true
+	$"No button rect/No button".disabled = true
+	dialogue = load_dialogue()
 
 func start() -> void:
+	if close_script == true:
+		close_script = false
+		return
 	if d_active:
 		return
+		
 	d_active = true
 	$NinePatchRect.visible = true
 	
-	dialogue = load_dialogue()
-	next_script()
+	if first_dialogue:
+		$NinePatchRect/Name.text = dialogue[current_dialogue_id]["name"]
+		$NinePatchRect/Chat.text = dialogue[current_dialogue_id]["text"]
+	else:
+		next_script()
 
 func load_dialogue():
 	if FileAccess.file_exists(d_file):
@@ -35,18 +48,31 @@ func load_dialogue():
 func _input(event: InputEvent) -> void:
 	if not d_active:
 		return
-	if event.is_action_pressed("interact") and first_dialogue == true:
-		dialogue.remove_at(0)
-		first_dialogue == false
-		return
-	elif event.is_action_pressed("interact") and first_dialogue == false:
-		next_script()
+		
+	if event.is_action_pressed("interact"):
+		if first_dialogue:
+			$NinePatchRect.visible = false
+			d_active = false
+			first_dialogue = false
+			dialogue.remove_at(0)
+			current_dialogue_id = 0
+			close_script = true
+		else:
+			next_script()
 		
 func next_script():
 	if current_dialogue_id >= len(dialogue):
 		d_active = false
 		$NinePatchRect.visible = false
+		current_dialogue_id = 0
 		return
-	
+		
+	$NinePatchRect.visible = true
 	$NinePatchRect/Name.text = dialogue[current_dialogue_id]["name"]
 	$NinePatchRect/Chat.text = dialogue[current_dialogue_id]["text"]
+	$"Yes button rect".visible = true
+	$"No button rect".visible = true
+	$"Yes button rect/Yes button".disabled = false
+	$"No button rect/No button".disabled = false
+	current_dialogue_id += 1
+	
--- Scripts/merchant.gd
+++ Scripts/merchant.gd
@@ -1,5 +1,6 @@
 extends StaticBody2D
 
+var first_dialogue = true
 var player_in_range = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -12,7 +13,14 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 		
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact") and player_in_range == true:
-		start_dialogue()
+		if first_dialogue == true:
+			first_dialogue = false
+			start_dialogue()
+			return
+		else:
+			await get_tree().create_timer(1).timeout
+			start_dialogue()
+			return
 		
 func start_dialogue():
 	$"../Merchant dialogue".start()
